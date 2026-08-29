8211f88379a654855bbda97a4d8fe7c841649489
yogesh Gadage <yogeshgadage@yogeshs-Mac-mini-2.local>
2026-08-30 01:57:26 +0530

    Use standard Next.js output for Hostinger

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: ".next-hostinger",
   output: "standalone",
   serverExternalPackages: ["mysql2"],
   experimental: { cpus: 1, webpackBuildWorker: false },
